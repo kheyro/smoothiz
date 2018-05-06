ea0c7725907846b8bf53e28e06d117e054bfdf1b
Denis Atkesone <denis.atkesone@gmail.com>
2018-05-06 16:47:56 +0200

    [ADD] Post signup route

--- config/routes.js
+++ config/routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const c = require('../app/controllers');
 
 router.route('/').get(c.application.getIndex);
+router.route('/signup').post(c.authentication.signUp);
 
 module.exports = router;
