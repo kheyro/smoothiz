cb68cc9750a841c9aa30fd245192ea4abfbcd9a6
Denis Atkesone <denis.atkesone@gmail.com>
2018-05-07 14:02:57 +0200

    [ADD] Authentication controller signup method

--- app/controllers/authentication.js
+++ app/controllers/authentication.js
@@ -1,6 +1,26 @@
+const User = require('../models/user');
+
 const authenticationController = {
-  signUp: (req, res) => {
-    res.send('signed up!');
+  signUp: (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(406)
+        .send({ error: 'You must provide email and password.' });
+    }
+
+    return User.forge({ email })
+      .fetch()
+      .then(model => {
+        if (model) {
+          return res.status(406).send({ error: 'email already in use.' });
+        }
+        return User.forge({ email, password })
+          .save()
+          .then(user => res.status(201).send({ success: true, user }))
+          .catch(err => next(err));
+      });
   },
 };
 
