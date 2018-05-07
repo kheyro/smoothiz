1f4596a0b5751b631483baddd04c3f6bc1ff4d50
Denis Atkesone <denis.atkesone@gmail.com>
2018-05-07 17:13:24 +0200

    [ADD] validatePassword method, refactor encryptPassword

--- app/models/user.js
+++ app/models/user.js
@@ -7,13 +7,24 @@ const User = bookshelf.Model.extend({
     this.on('creating', this.encryptPassword);
   },
   encryptPassword: model =>
-    new Promise((resolve, reject) =>
-      bcrypt.hash(model.attributes.password, 10, (err, hash) => {
-        if (err) reject(err);
-        model.set('password', hash);
-        resolve(hash);
-      })
-    ),
+    bcrypt
+      .hash(model.attributes.password, 10)
+      .then(hash => model.set('password', hash))
+      .catch(err => err),
+  // encryptPassword: model =>
+  //   new Promise((resolve, reject) =>
+  //     bcrypt.hash(model.attributes.password, 10, (err, hash) => {
+  //       if (err) reject(err);
+  //       model.set('password', hash);
+  //       resolve(hash);
+  //     })
+  //   ),
+  validatePassword(suppliedPassword) {
+    return bcrypt
+      .compare(suppliedPassword, this.attributes.password)
+      .then(res => res)
+      .catch(err => err);
+  },
 });
 
 module.exports = bookshelf.model('User', User);
