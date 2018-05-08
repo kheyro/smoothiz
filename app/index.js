6de6540d5ad75767289d0920456f9e66cbf09f43
Denis Atkesone <denis.atkesone@gmail.com>
2018-05-08 14:36:40 +0200

    [ADD] app use cors middleware

--- app/index.js
+++ app/index.js
@@ -4,12 +4,14 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const routes = require('../config/routes');
 
 const app = express();
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' })); // { type: '*/*' }
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
