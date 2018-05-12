ac613827b25b5757388b431295ef73ee83634e7d
Denis Atkesone <denis.atkesone@gmail.com>
2018-05-12 23:57:31 +0200

    [ADD] create Smoothy model

---
+++ app/models/smoothy.js
@@ -0,0 +1,16 @@
+const bookshelf = require('bookshelf');
+
+const User = require('./user');
+const Category = require('./category');
+
+const Smoothy = bookshelf.Model.extend({
+  tableName: 'smoothies',
+  users: function () {
+    this.belongsTo(User);
+  },
+  categories: function() {
+    this.belongsToMany(Category);
+  },
+});
+
+module.exports = bookshelf.model('Smoothy', Smoothy);
