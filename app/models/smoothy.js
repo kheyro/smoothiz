3cdceadd56385f577a79446a90b02a258c1bafa1
Denis Atkesone <denis.atkesone@gmail.com>
2018-05-13 15:53:43 +0200

    [FIX] add return to relation tables and imported bookshelf

--- app/models/smoothy.js
+++ app/models/smoothy.js
@@ -1,4 +1,4 @@
-const bookshelf = require('bookshelf');
+const bookshelf = require('../../config/bookshelf');
 
 const User = require('./user');
 const Category = require('./category');
@@ -6,11 +6,11 @@ const Category = require('./category');
 const Smoothy = bookshelf.Model.extend({
   tableName: 'smoothies',
   hasTimestamps: true,
-  users: function () {
-    this.belongsTo(User);
+  users: function() {
+    return this.belongsTo(User);
   },
   categories: function() {
-    this.belongsToMany(Category);
+    return this.belongsToMany(Category, 'smoothies_categories');
   },
 });
 
