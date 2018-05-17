8247c2c12270506746294cc0539e27441b9b0e58
Denis Atkesone <denis.atkesone@gmail.com>
2018-05-17 15:38:43 +0200

    [ADD] add dependents property for cascasde deletion

--- app/models/smoothy.js
+++ app/models/smoothy.js
@@ -3,15 +3,20 @@ const bookshelf = require('../../config/bookshelf');
 const User = require('./user');
 const Category = require('./category');
 
-const Smoothy = bookshelf.Model.extend({
-  tableName: 'smoothies',
-  hasTimestamps: true,
-  users: function() {
-    return this.belongsTo(User);
+const Smoothy = bookshelf.Model.extend(
+  {
+    tableName: 'smoothies',
+    hasTimestamps: true,
+    users: function() {
+      return this.belongsTo(User);
+    },
+    categories: function() {
+      return this.belongsToMany(Category, 'smoothies_categories');
+    },
   },
-  categories: function() {
-    return this.belongsToMany(Category, 'smoothies_categories');
-  },
-});
+  {
+    dependents: ['categories'],
+  }
+);
 
 module.exports = bookshelf.model('Smoothy', Smoothy);
