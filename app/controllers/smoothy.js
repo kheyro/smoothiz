34854995dbcefe33218ce8022f107b4ca1869475
Denis Atkesone <denis.atkesone@gmail.com>
2018-05-17 12:54:00 +0200

    [ADD] create editSmoothie controller + user_id check

--- app/controllers/smoothy.js
+++ app/controllers/smoothy.js
@@ -15,6 +15,40 @@ const smoothyController = {
       })
       .catch(err => next(err));
   },
+  editSmoothie: (req, res, next) => {
+    const { name, description, recipe, visibility } = req.body;
+    const smoothieId = req.params.id;
+    const user_id = req.user.id; // user data sent from passport
+    const category_ids = req.body.categoryIds;
+    // make sure it belongs to the right user requesting change
+    Smoothy.forge({ id: smoothieId })
+      .fetch()
+      .then(smt => {
+        if (smt.attributes.user_id !== user_id) res.sendStatus(401);
+      })
+      .catch(err => next(err));
+    Smoothy.forge({
+      id: smoothieId,
+      name,
+      description,
+      recipe,
+      visibility,
+      user_id: 1,
+    })
+      .save()
+      .then(smoothy => {
+        // Delete all categories and re-add them
+        smoothy
+          .categories()
+          .detach()
+          .then(() => smoothy.categories().attach(category_ids))
+          .catch(err => next(err));
+        res.status(200).send({
+          smoothy: smoothy.id,
+        });
+      })
+      .catch(err => next(err));
+  },
 };
 
 module.exports = smoothyController;
