3b7e487d3f571e0736b91491548fd91d17c8ce82
Denis Atkesone <denis.atkesone@gmail.com>
2018-05-17 13:50:15 +0200

    [ADD] add bookshelf-cascade-delete to take care of cascade deletion

--- config/bookshelf.js
+++ config/bookshelf.js
@@ -1,6 +1,8 @@
 const knex = require('knex')(require('../knexfile')[process.env.NODE_ENV]);
 const bookshelf = require('bookshelf')(knex);
+const cascadeDelete = require('bookshelf-cascade-delete');
 
 bookshelf.plugin('registry');
+bookshelf.plugin(cascadeDelete);
 
 module.exports = bookshelf;
