e00537c37a6cae2ba8b9ec1fec7ca6fc060fa64d
Denis Atkesone <denis.atkesone@gmail.com>
2018-05-20 18:26:09 +0200

    [FIX] add unique directive for user_id/smoothy_id

--- migrations/20180519174224_create_likes.js
+++ migrations/20180519174224_create_likes.js
@@ -4,6 +4,7 @@ exports.up = (knex, Promise) =>
       tbl.increments();
       tbl.integer('user_id').references('users.id');
       tbl.integer('smoothy_id').references('smoothies.id');
+      tbl.unique(['user_id', 'smoothy_id']);
     }),
   ]);
 
