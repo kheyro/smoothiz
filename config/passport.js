9b978a16e2de4bb3237abea23f4f426fdea1c29f
Denis Atkesone <denis.atkesone@gmail.com>
2018-05-21 13:25:44 +0200

    [ADD] add Facebook Strategy

--- config/passport.js
+++ config/passport.js
@@ -3,6 +3,7 @@ const User = require('../app/models/user');
 const JwtStrategy = require('passport-jwt').Strategy;
 const { ExtractJwt } = require('passport-jwt');
 const LocalStrategy = require('passport-local');
+const FacebookStrategy = require('passport-facebook');
 
 const localOptions = { usernameField: 'email' };
 
@@ -43,5 +44,37 @@ const jwtLogin = new JwtStrategy(jwtOptions, (payload, done) => {
     .catch(err => done(err, false));
 });
 
+const facebookLogin = new FacebookStrategy(
+  {
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    profileFields: [
+      'id',
+      'email',
+      'name',
+      'birthday',
+      'gender',
+      'picture',
+      'profileUrl',
+      'displayName',
+    ],
+  },
+  (accessToken, refreshToken, profile, done) => {
+    console.log('accessTOKEN', accessToken, profile, done);
+    User.forge({ id: 1 })
+      .fetch()
+      .then(user => {
+        if (user) {
+          done(null, user);
+        } else {
+          done(null, false);
+        }
+      })
+      .catch(err => done(err, false));
+  }
+);
+
 passport.use(jwtLogin);
 passport.use(localLogin);
+passport.use(facebookLogin);
