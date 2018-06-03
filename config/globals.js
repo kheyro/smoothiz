d3feb6702b7ff64a0271ed6bd433cdbb2b8afc43
Denis Atkesone <denis.atkesone@gmail.com>
2018-06-03 19:48:57 +0200

    [UP] change global depending on NODE_ENV

--- config/globals.js
+++ config/globals.js
@@ -1,4 +1,10 @@
 module.exports = {
-  API_SERVER: 'http://localhost:3000',
-  CLIENT_SERVER: 'http://localhost:8080',
+  API_SERVER:
+    process.env.NODE_ENV === 'production'
+      ? 'https://smoothiz-api-server.herokuapp.com/'
+      : 'http://localhost:3000',
+  CLIENT_SERVER:
+    process.env.NODE_ENV === 'production'
+      ? 'https://smoothiz.herokuapp.com/'
+      : 'http://localhost:8080',
 };
