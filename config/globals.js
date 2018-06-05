82f565b5ecbefa98891a1e7fce1548c0b2400d2c
Denis Atkesone <denis.atkesone@gmail.com>
2018-06-05 11:09:23 +0200

    [FIX] remove trailing slash on url

--- config/globals.js
+++ config/globals.js
@@ -1,10 +1,10 @@
 module.exports = {
   API_SERVER:
     process.env.NODE_ENV === 'production'
-      ? 'https://smoothiz-api-server.herokuapp.com/'
+      ? 'https://smoothiz-api-server.herokuapp.com'
       : 'http://localhost:3000',
   CLIENT_SERVER:
     process.env.NODE_ENV === 'production'
-      ? 'https://smoothiz.herokuapp.com/'
+      ? 'https://smoothiz.herokuapp.com'
       : 'http://localhost:8080',
 };
