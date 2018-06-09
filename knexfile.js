ba570943289090ae8ccfcad400101cb5ec41601f
Denis Atkesone <denis.atkesone@gmail.com>
2018-06-09 09:52:16 +0200

    [RF] remove stating property

--- knexfile.js
+++ knexfile.js
@@ -20,23 +20,6 @@ module.exports = {
       directory: './db/migrations',
     },
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
-  },
-
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
